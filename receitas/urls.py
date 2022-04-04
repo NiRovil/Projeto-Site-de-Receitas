f326e245546e2237708183c3efb33e5ef4229488
NiRovil <nicolas199851@hotmail.com>
2022-04-03 22:04:08 -0300

    testes de url

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('receita<int:receitas_id>', views.receita, name='receita')
+    path('<int:receitas_id>', views.receita, name='receita')
 ]
