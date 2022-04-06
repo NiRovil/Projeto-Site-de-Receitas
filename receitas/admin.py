4b1ddb5c264fa6e83e11c293963a1155b558db3f
NiRovil <nicolas199851@hotmail.com>
2022-04-06 20:32:26 -0300

    adicionando_novas_funcoes

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,5 +1,12 @@
+from re import search
 from django.contrib import admin
-from .models import Receita
+from . models import Receita
 # Register your models here.
 
-admin.site.register(Receita)
+class ModeloRegistro(admin.ModelAdmin):
+    list_display = ['id', 'nome_receita']
+    list_display_links = ['id', 'nome_receita']
+    list_filter = ['categoria']
+    search_fields = ['nome_receita']
+
+admin.site.register(Receita, ModeloRegistro)
