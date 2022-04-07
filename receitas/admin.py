71a8d947d45d7e1ec22a08e722bb3f3e18f20963
NiRovil <nicolas199851@hotmail.com>
2022-04-06 22:50:50 -0300

    Adicionando link de pessoas

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Receita
 # Register your models here.
 
-admin.site.register(Receita)
+class ModeloRegistro(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita')
+    list_display_links = ('id', 'nome_receita')
+    list_filter = ['categoria']
+    search_fields = ['nome_receita']
+    list_per_page = 10
+
+admin.site.register(Receita, ModeloRegistro)
